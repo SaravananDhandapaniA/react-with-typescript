b96d72da7a48b003db35ec6a6376685f2141cc53
SaravananDhandapaniA <saravanan@tringapps.com>
2021-10-12 11:03:20 +0530

    firstcommit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from "./components/List"
+import AddToList from './components/AddToList';
+
+export interface IState {
+  people: {
+    name:string
+    age:number
+    url:string
+    note?:string
+  }[]
+}
 
 function App() {
+
+  const [people, setPeople]=useState<IState["people"]>([
+    {
+      name:"Virat Kohli",
+      url:"https://english.cdn.zeenews.com/sites/default/files/styles/zm_700x400/public/2021/08/18/961806-kohli-970.jpg",
+      age:32,
+      note:"He is the world best batsman in this current generation"
+    },
+    {
+      name:"MS Dhoni",
+      url:"https://i.pinimg.com/474x/e0/c6/57/e0c65706c7aea5e34421c0736a8a6f25.jpg",
+      age:39,
+      note:"Captain Cool"
+    }
+  ])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Players in Good Form</h1>
+      <List people={people}/>
+      <AddToList people={people} setPeople={setPeople}/>
     </div>
   );
 }
